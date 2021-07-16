9b26854631e828cd880f9785ec576e96fef9f1e3
Micu96Padron <padronmica@gmail.com>
2021-07-16 20:17:47 +0200

    Arreglo importante

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os
 import urllib
 
-from google.appengine.api import users
-from google.appengine.ext import ndb
-
 import jinja2
 import webapp2
+from google.appengine.api import users
+from google.appengine.ext import ndb
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),

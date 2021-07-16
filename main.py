4219953736bff7264feb6bd8e2198ed465a8d968
Micu96Padron <padronmica@gmail.com>
2021-07-16 20:21:05 +0200

    Arreglo importante

--- main.py
+++ main.py
@@ -19,13 +19,11 @@ def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
 
 
 class Author(ndb.Model):
-    """Sub model for representing an author."""
     identity = ndb.StringProperty(indexed=False)
     email = ndb.StringProperty(indexed=False)
 
 
 class Greeting(ndb.Model):
-    """A main model for representing an individual Guestbook entry."""
     author = ndb.StructuredProperty(Author)
     content = ndb.StringProperty(indexed=False)
     date = ndb.DateTimeProperty(auto_now_add=True)

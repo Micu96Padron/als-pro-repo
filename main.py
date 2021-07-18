46b1efebe5850ab70bd61e916f4509f5fd7e1bc2
Micu96Padron <padronmica@gmail.com>
2021-07-18 12:52:37 +0200

    Default game addition

--- main.py
+++ main.py
@@ -12,16 +12,23 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+DEFAULT_GAME_NAME = 'The Legend of Zelda: Breath of the Wild'
+DEFAULT_GAME_GENRES = ['Action', 'Adventure']
+
+
 def get_default_name():
-    toret = ''
+    act_games = Game.query()
+    if act_games.count() == 0:
+        game = Game(name=DEFAULT_GAME_NAME, genres=DEFAULT_GAME_GENRES)
+        game.put()
+        act_games = Game.query()
+
+    toret = act_games[0].name
 
     return toret
 
 
-DEFAULT_GAME_NAME = get_default_name()
-
-
-def guestbook_key(game_name=DEFAULT_GAME_NAME):
+def guestbook_key(game_name=get_default_name()):
     return ndb.Key('Game', game_name)
 
 
@@ -29,9 +36,11 @@ class Author(ndb.Model):
     identity = ndb.StringProperty(indexed=False)
     email = ndb.StringProperty(indexed=False)
 
+
 class Game(ndb.Model):
     name = ndb.StringProperty(required=True)
     genre = ndb.StringProperty(repeated=True)
+
 
 class Review(ndb.Model):
     author = ndb.StructuredProperty(Author)
@@ -77,8 +86,8 @@ class ReviewForum(webapp2.RequestHandler):
 
         if users.get_current_user():
             greeting.author = Author(
-                    identity=users.get_current_user().user_id(),
-                    email=users.get_current_user().email())
+                identity=users.get_current_user().user_id(),
+                email=users.get_current_user().email())
 
         greeting.content = self.request.get('content')
         greeting.put()
